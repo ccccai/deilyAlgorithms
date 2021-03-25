75fbc4f13acd9ae5c6294baa5ad4bb41cf9b5f79
caishiyin <caisy8@midea.com>
2021-03-25 14:22:43 +0800

    feature：最长公共前缀  加注释

--- 20210325/longest-common-prefix.js
+++ 20210325/longest-common-prefix.js
@@ -1,7 +1,7 @@
 /*
  * @Author: caishiyin
  * @Date: 2021-03-25 10:19:36
- * @LastEditTime: 2021-03-25 14:06:44
+ * @LastEditTime: 2021-03-25 14:22:17
  * @LastEditors: caishiyin
  * @Description: 
  * @FilePath: \dailyAlgorithms\20210325\longest-common-prefix.js
@@ -15,11 +15,12 @@ const longestCommonPrefix = (strs) => {
   const str = strs.reduce((res, next) => res.length > next ? next : res) // 找出最短字符串
 
   for (let i = 1; i <= str.length; i++) {
+    // 遍历最短字符串，看到哪个字符为止就有差异
     if (strs.some(item => !(item.substring(0, i) === str.substring(0, i)))) {
       result = str.substring(0, i - 1)
       break;
     } else {
-      result = str // 
+      result = str // 全部一致
     }
   }
 
